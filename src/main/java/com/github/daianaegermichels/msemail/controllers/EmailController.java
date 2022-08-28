7ae48099fc88cb32e896275b1db19d7190114317
Daiana Michels <daianaem.12@gmail.com>
2022-08-28 20:46:28 -0300

    feat: configure post method for sending emails

--- src/main/java/com/github/daianaegermichels/msemail/controllers/EmailController.java
+++ src/main/java/com/github/daianaegermichels/msemail/controllers/EmailController.java
@@ -1,7 +1,32 @@
 package com.github.daianaegermichels.msemail.controllers;
 
+import com.github.daianaegermichels.msemail.dtos.EmailDto;
+import com.github.daianaegermichels.msemail.models.EmailModel;
+import com.github.daianaegermichels.msemail.services.EmailService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class EmailController {
+
+    final
+    EmailService emailService;
+
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping("/sending-email")
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto){
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/github/daianaegermichels/msemail/services/EmailService.java
+++ src/main/java/com/github/daianaegermichels/msemail/services/EmailService.java
@@ -1,13 +1,46 @@
 package com.github.daianaegermichels.msemail.services;
 
+import com.github.daianaegermichels.msemail.enums.StatusEmail;
+import com.github.daianaegermichels.msemail.models.EmailModel;
 import com.github.daianaegermichels.msemail.repositories.EmailRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
 
-    @Autowired
+    final
     EmailRepository emailRepository;
 
+    final JavaMailSender emailSender;
+
+    public EmailService(EmailRepository emailRepository, JavaMailSender emailSender) {
+        this.emailRepository = emailRepository;
+        this.emailSender = emailSender;
+    }
+
+    public EmailModel sendEmail(EmailModel emailModel) {
+
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+
+        }catch (MailException e){
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+
+        } finally {
+            return emailRepository.save(emailModel);
+        }
+    }
 }
